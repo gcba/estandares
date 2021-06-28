08461a5f6e85f15837fdf6d36439ae2046cfd76d
Nicolás Rougier <narougier@gmail.com>
2021-06-27 23:34:10 -0300

    Ajustes generales en fichas de componentes

--- src/components/docs/Card.tsx
+++ src/components/docs/Card.tsx
@@ -1,43 +1,20 @@
 import * as React from "react"
-import { withPrefix, navigate } from "gatsby"
+import { withPrefix } from "gatsby"
+import { Card as BaseCard, SimpleCard as BaseSimpleCard, CardProps, SimpleCardProps } from "@gcba/obelisco"
 
-interface BaseCardProps {
-  title: string
-  description: string
-  linkUrl: string
-  icon?: {
-    name: string
-    color: string
+export const Card = (props: React.PropsWithChildren<CardProps>) => {
+  addPrefix(props.href);
+  addPrefix(props.picture?.src);
+  return <BaseCard {...props} />
+}
+
+export const SimpleCard = (props: React.PropsWithChildren<SimpleCardProps>) => {
+  addPrefix(props.href);
+  return <BaseSimpleCard {...props} />
+}
+
+const addPrefix = (url?: string) => {
+  if(url && !url.includes("//")) {
+    url = withPrefix(url);
   }
 }
-
-interface CardProps extends BaseCardProps {
-  img?: string
-}
-
-export const Card = (props: React.PropsWithChildren<CardProps>) => (
-  <button className="card" onClick={() => navigate(props.linkUrl)}>
-    {props.img && <img src={withPrefix(props.img)} className="card-img-top" />}
-    <div className="card-body">
-      {props.icon && <i className={`card-icon bx ${props.icon.name} `} style={{ color: props.icon.color }}></i>}
-      <h4 className="card-title">{props.title}</h4>
-      <p className="card-text">{props.description}</p>
-    </div>
-  </button>
-)
-
-interface SimpleCardProps extends BaseCardProps {
-  linkText: string
-}
-
-export const SimpleCard = (props: React.PropsWithChildren<SimpleCardProps>) => (
-  <div className="card card-simple">
-    <div className="card-body">
-      {props.icon && <i className={`card-icon bx ${props.icon.name} `} style={{ color: props.icon.color }}></i>}
-      <h4 className="card-title">
-        <a href={props.linkUrl.includes("//") ? props.linkUrl : withPrefix(props.linkUrl)}>{props.title}</a>
-      </h4>
-      <p className="card-text">{props.description}</p>
-    </div>
-  </div>
-)
---
+++ src/components/docs/Progression.tsx
@@ -0,0 +1,37 @@
+import * as React from "react"
+
+export const ProgessionExample = () => {
+  const [isOpen, setOpen] = React.useState<Boolean>(false);
+  
+  return <div className="progression-container">
+    <ul className={`progression ${isOpen ? "expanded" : ""}`}>
+      <li className="progression-item completed" aria-expanded="false">
+        <p className="progression-title">Estado anterior</p>
+        <p className="progression-subtitle">Descripción del estado dd/mm/aa - 00:00hs</p>
+      </li>
+
+      <li className="progression-item active" aria-expanded="true">
+        <p className="progression-title">Estado actual</p>
+        <p className="progression-subtitle">Descripción del estado dd/mm/aa - 00:00hs</p>
+      </li>
+
+      <li className="progression-item" aria-expanded="false">
+        <p className="progression-title">Próximo estado</p>
+        <p className="progression-subtitle">Descripción del estado dd/mm/aa - 00:00hs</p>
+      </li>
+
+      <li className="progression-item" aria-expanded="false">
+        <p className="progression-title">Próximo estado</p>
+        <p className="progression-subtitle">Descripción del estado dd/mm/aa - 00:00hs</p>
+      </li>
+
+      <li className="progression-item" aria-expanded="false">
+        <p className="progression-title">Estado final</p>
+        <p className="progression-subtitle">Descripción del estado dd/mm/aa - 00:00hs</p>
+      </li>
+    </ul>
+
+    <button className="progression-link" onClick={() => setOpen(!isOpen)}>
+      {isOpen ? "Menos" : "Más"} detalles
+    </button>
+  </div>}
