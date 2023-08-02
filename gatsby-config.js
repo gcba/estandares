60749f48c6dc80bd57c70ee28c1418ae8aaed0ab
Victoria Nastasi <victorianastasi@gmail.com>
2023-08-02 11:49:15 -0300

    fix: cambio de favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/logo.png`, // This path is relative to the root of the site.
+        icon: `src/images/obelisco.png`, // This path is relative to the root of the site.
       },
     },
     {
