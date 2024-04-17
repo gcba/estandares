244757f61d724feebfe663042f8f661743d7e151
Victoria Rodriguez <victoriaileanarodriguezmora@gmail.com>
2024-04-17 10:10:53 -0300

    feat(src\components\includes\TableOfContent.tsx): :sparkles: Se agrega Alerta en la página Paleta de Colores.

--- src/components/includes/TableOfContent.tsx
+++ src/components/includes/TableOfContent.tsx
@@ -10,6 +10,13 @@ interface TableOfContentsProps {
 export const TableOfContents = (props: TableOfContentsProps) => {
   return (
     <div className="contents">
+
+      <div className="alert alert-primary mb-5" role="alert">
+        <p>
+          <strong>Esta guía de estilo se encuentra en revisión.</strong> Estamos trabajando en la paleta de colores, por lo que surgirán cambios que afectarán a todo el sistema de diseño.
+        </p>
+      </div>
+
       <div className="contents-title">Contenido en esta página</div>
       <ul className="list-unstyled">
         {props.items.map(item => (
